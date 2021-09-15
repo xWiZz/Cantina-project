f90e5a70cf5bb00685bd93d551754d67a8daf835
Vincent Cuvilliez <vcn.cvz@icloud.com>
2021-09-15 11:30:18 +0200

    Initialize project

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
 import './App.css';
-import { useState, useEffect } from 'react'; 
+import {BrowserRouter, NavLink, Route} from 'react-router-dom';
+
+import Recette from './views/Recette';
+import Accueil from './views/Accueil';
 
 function App() {
-
-  const [recettes, setRecettes] = useState(null);
-
-  useEffect(() => {
-    fetch('http://localhost:9000/api/recipes')
-    .then(res => res.json())
-    .then(recipes => {
-      setRecettes(recipes);
-    });
-  }, []);
-
   return (
-    <div className="App">
-      <h1>Liste des recettes</h1>
+    <BrowserRouter>
+      <div className="App">
+        <header>
+          <ul>
+            <li><NavLink to ="/" exact>Accueil</NavLink></li>
+          </ul>
+        </header>
 
-      {recettes && recettes.map(recette =>
-      <div key={recette.id}>
-        <h2>{recette.titre}</h2>
-        <img src={recette.photo} alt="" />
-        <p>Niveau : {recette.niveau}</p>
-        <p>Nombre de personne : {recette.personnes}</p>
-        <p>Description : {recette.description}</p>
-        <p>Temps de préparation : {recette.tempsPreparation}</p>
+        <Route path="/" component={Accueil} exact />
+        <Route path="/recette/:id" component={Recette} />
       </div>
-      )}
-    </div>
+    </BrowserRouter>
   );
 }
 
